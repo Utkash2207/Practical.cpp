6f015492c0e8f5aba6f450a0dc01710f0d1ed017
Utkarsh2207 <161607722+Utkash2207@users.noreply.github.com>
2024-05-21 02:21:15 +0530

    practical_09.cpp

--- practical_09.cpp
+++ practical_09.cpp
@@ -1,34 +1,14 @@
-#include <iostream>
-#include <string>
+// WAP that accepts numbers from keyword and find its factorial.
+# include <iostream>
+using namespace std;
 
-// Define a structure for a book
-struct Book {
-    std::string title;
-    std::string authorName;
-    std::string publication;
-    float price;
-};
-
-int main() {
-    Book book;
-
-    // Prompt the user to enter details of the book
-    std::cout << "Enter details of the book:\n";
-    std::cout << "Title: ";
-    std::getline(std::cin >> std::ws, book.title);
-    std::cout << "Author Name: ";
-    std::getline(std::cin >> std::ws, book.authorName);
-    std::cout << "Publication: ";
-    std::getline(std::cin >> std::ws, book.publication);
-    std::cout << "Price: ";
-    std::cin >> book.price;
-
-    // Display the book record
-    std::cout << "\nBook Record:\n";
-    std::cout << "Title: " << book.title << std::endl;
-    std::cout << "Author Name: " << book.authorName << std::endl;
-    std::cout << "Publication: " << book.publication << std::endl;
-    std::cout << "Price: " << book.price << std::endl;
-
+int main(){
+    int number,fact=1;
+    cout<<"enter any positive number :";
+    cin>>number;
+    for(int i=1;i<=number;i++){
+        fact*=i;
+    }
+    cout<<"Factorial of number is :"<<fact;
     return 0;
 }
