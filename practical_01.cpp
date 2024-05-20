b544d9d8f9b1e8e068863384aaecf22c93b070eb
Utkarsh2207 <161607722+Utkash2207@users.noreply.github.com>
2024-05-21 02:09:04 +0530

    practical_01.cpp

--- practical_01.cpp
+++ practical_01.cpp
@@ -6,11 +6,10 @@ int main(){
     int a,b,sum;
     cout<<"enter the value of a :";
     cin>>a;
-    cout<<"enter the value of B :";
+    cout<<"enter the value of b :";
     cin>>b;
     sum = a+b;
     cout<<"Sum of two Number = "<<sum;
 
     return 0;
-
 }
